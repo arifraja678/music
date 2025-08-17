5ca82554e9a58c0394cae2864d66593a5f74b9bc
arifraja678 <arifaja2847@gmail.com>
2025-08-17 08:34:13 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002396985894"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", " -1002195149007"))
 
 
 # Your User ID.
